c684eb7f54e03ae19069ba7d0e8aab1b1625aa89
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-14 00:09:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+ namespace Lists
+{
+    class List
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>(); // creating the instance for the our list
+
+            list.Add("1 - Bülent Ersoy");
+            list.Add("2 - Ajda Pekkan");
+            list.Add("3 - Ebru Gündeş");
+            list.Add("4 - Hadise");
+            list.Add("5 - Hande Yener");
+            list.Add("6 - Tarkan");
+            list.Add("7 - Funda Arar");
+            list.Add("8 - Demet Akalın");
+            Console.WriteLine("** Davetliler **"); // Static string for the our program
+            foreach (var item in list)
+            {
+               
+                Console.WriteLine(item); // Write to the console each item
+            }
+
+        }
+    }
+
+
+}
